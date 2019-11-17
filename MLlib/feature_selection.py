f23443a42378cd7f6d038347db68f9b1d19df304
Bhargav Panth <bhargavrpanth@gmail.com>
2019-11-17 01:34:16 +0000

    added df assembler for feature extraction

--- MLlib/feature_selection.py
+++ MLlib/feature_selection.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.ml.feature import VectorAssembler
 import sys
 sys.path.append('src/Consumer/')
 from consumer import Consumer
@@ -18,5 +19,15 @@ class FeatureSelection:
         departure
         temperature
         '''
-        pass
+        rdd = self.stream.filter(lambda message: is_number(message)) \
+            .map(lambda message: ( round(float(message) * 2, 0) / 2, 1 )) \
+            .transform(lambda rdd: rdd.sortByKey())
+        assembler = VectorAssembler(inputCols = ['stop_id', 'delay', 'route_id', 'departure'], outputCol = 'features')
+        assembler.transform(rdd)
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
