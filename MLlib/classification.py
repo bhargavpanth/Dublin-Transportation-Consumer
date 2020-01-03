856dc4a3bffb3c6b526ed21e40b805259c6f711f
Bhargav Panth <bhargav@sphere.me>
2020-01-03 00:21:56 +0000

    adding mllib spark classifier

--- MLlib/classification.py
+++ MLlib/classification.py
@@ -3,3 +3,8 @@ from pyspark.streaming import StreamingContext
 import sys
 sys.path.append('src/Consumer/')
 from consumer import Consumer
+
+class Classification:
+    def __init__(self):
+        pass
+
