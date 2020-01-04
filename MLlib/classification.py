d592d0b80e4daeaec6f9530327039e32af0c16f1
Bhargav Panth <bhargavrpanth@gmail.com>
2020-01-04 00:11:14 +0000

    experimenting with logistic regression

--- MLlib/classification.py
+++ MLlib/classification.py
@@ -10,3 +10,9 @@ class Classification:
         self.consumer = Consumer('bus', 'localhost')
         self.stream = self.consumer.get_stream()
 
+    def logistic_regression(self):
+        rdd = self.stream.filter(lambda message: is_number(message)) \
+            .map(lambda message: round(float(message))) \
+            .transform(lambda rdd: rdd.sortByKey())
+        pass
+
