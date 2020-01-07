90296ee7da0f129b29af48cebe8346fbe4539647
Bhargav Panth <bhargavrpanth@gmail.com>
2020-01-07 22:35:08 +0000

    WIP - trying to clean up DStream data

--- MLlib/regression.py
+++ MLlib/regression.py
@@ -8,8 +8,8 @@ class Regression:
     def __init__(self):
         self.consumer = Consumer('bus', 'localhost')
         self.stream = self.consumer.get_stream()
+        self.cleaned_stream = self.stream.map(self.clean_up)
 
     def clean_up(self):
         pass
-
 
