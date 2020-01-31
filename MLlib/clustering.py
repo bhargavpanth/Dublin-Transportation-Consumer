1acb87bc53d10a7a84d34c4e20271ac20afe77a3
Bhargav Panth <bhargavrpanth@gmail.com>
2020-01-31 00:51:45 +0000

    adding streaming specifics

--- MLlib/clustering.py
+++ MLlib/clustering.py
@@ -1,5 +1,9 @@
 from pyspark.sql import SparkSession
+sys.path.append('src/Consumer/')
+from consumer import Consumer
 
 class KMeans:
     def __init__(self):
         self.spark = SparkSession.builder.appName('kmeans').getOrCreate()
+        self.consumer = Consumer('bus', 'localhost')
+        self.stream = self.consumer.get_stream()
